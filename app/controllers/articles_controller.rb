c8cc952c623a4969b19e68b602e3cb3ba96e077e
mihir <mlele@redhat.com>
2017-11-16 16:05:32 +0000

    added user_id to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
    
    def create
    @article = Article.new(article_params)
+   @article.user = User.first
       if @article.save
          flash[:success] = "Articles was successfully created!!" 
          redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates :username, presence: true, length: { minimum: 3, maximum: 35 }, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { minimum: 5, maximum: 30 }, uniqueness: { case_sensitive: false }, 
